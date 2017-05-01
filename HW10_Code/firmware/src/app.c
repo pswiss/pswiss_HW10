633d7d2c307aab6798d3ec1e2f0f9f526cf337bf
pswiss <petrasswissler2022@u.northwestern.edu>
2017-04-30 21:20:21 -0500

    MAF Implemented

--- HW10_Code/firmware/src/app.c
+++ HW10_Code/firmware/src/app.c
@@ -1,51 +1,3 @@
-/*******************************************************************************
-  MPLAB Harmony Application Source File
-  
-  Company:
-    Microchip Technology Inc.
-  
-  File Name:
-    app.c
-
-  Summary:
-    This file contains the source code for the MPLAB Harmony application.
-
-  Description:
-    This file contains the source code for the MPLAB Harmony application.  It 
-    implements the logic of the application's state machine and it may call 
-    API routines of other MPLAB Harmony modules in the system, such as drivers,
-    system services, and middleware.  However, it does not call any of the
-    system interfaces (such as the "Initialize" and "Tasks" functions) of any of
-    the modules in the system or make any assumptions about when those functions
-    are called.  That is the responsibility of the configuration-specific system
-    files.
- *******************************************************************************/
-
-// DOM-IGNORE-BEGIN
-/*******************************************************************************
-Copyright (c) 2013-2014 released Microchip Technology Inc.  All rights reserved.
-
-Microchip licenses to you the right to use, modify, copy and distribute
-Software only when embedded on a Microchip microcontroller or digital signal
-controller that is integrated into your product or third party product
-(pursuant to the sublicense terms in the accompanying license agreement).
-
-You should refer to the license agreement accompanying this Software for
-additional information regarding your rights and obligations.
-
-SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
-EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
-MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
-IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
-CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
-OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
-INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
-CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
-SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
-(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
- *******************************************************************************/
-// DOM-IGNORE-END
-
 
 // *****************************************************************************
 // *****************************************************************************
@@ -61,6 +13,7 @@ SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
 #include"I2C2_Commands.h"
 
 #define messageLen 100
+#define MAFsize 4
 #define CS LATBbits.LATB7       // chip select pin
 
 // Global Variables
@@ -69,6 +22,10 @@ char freqUpdate = 100;
 char maxNum = 100+1;
 
 char message[messageLen];
+
+short MAFarray[]={0,0,0,0};
+char iMAF = 0;
+float MAFval = 0;
 
 
 // Definitions to make life easier
@@ -838,14 +795,29 @@ void APP_Tasks(void) {
                 short accelX        = (data[9] << 8)    | data[8];
                 short accelY        = (data[11] << 8)   | data[10];
                 short accelZ        = (data[13] << 8)   | data[12];
+                
+                iMAF++;
+                if(iMAF >= MAFsize)
+                {
+                    iMAF = 0;
+                }
+                MAFarray[iMAF] = accelZ;
+                int k;
+                int sum = 0;
+                for(k=0;k<MAFsize;k++)
+                {
+                    sum = sum+MAFarray[k];
+                }
+                MAFval = 1.00*((float)sum)/MAFsize;
                      
-                len = sprintf(dataOut, "%d %d  \r\n", i,accelZ);
+                len = sprintf(dataOut, "%d %d %f  \r\n", i,accelZ, MAFval);
             }
 
             if (appData.isReadComplete) {
                 // Check if an 'r' was pushed
                 if (appData.readBuffer[0] == 'r') {
                     i = 0;
+                    
                 }
                 // Report observed letter
                 USB_DEVICE_CDC_Write(USB_DEVICE_CDC_INDEX_0,
